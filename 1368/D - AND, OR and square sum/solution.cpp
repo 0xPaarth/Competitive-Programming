3d30a47898cc62d01f144d49f085e1d7f9e42937
Paarth Arora <iit2025280@iiita.ac.in>
2026-06-29 12:34:38 +0530

    Add AND, OR and square sum [D] from Codeforces

---
+++ 1368/D - AND, OR and square sum/solution.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll =long long;
+int main()
+{
+	int n;
+	cin>>n;
+	vector <ll> arr(n);
+	for(int i=0;i<n;i++)
+		cin>>arr[i];
+     for(int bit=0;bit<20;bit++)
+       {
+	int ones=0;
+	ll mask=(1LL<<bit);
+	for(ll a:arr)
+	{
+		if((a&mask)!=0)
+			ones++;
+	}
+	for(int i=0;i<ones;i++)
+		arr[i]|=mask;
+	for(int i=ones;i<n;i++)
+	{
+		if((arr[i]&mask)!=0)
+			arr[i]=arr[i]&(~mask);
+	}
+}
+ll res=0;
+for(ll a:arr)
+	res+=a*a;
+cout<<res<<endl;
+}
